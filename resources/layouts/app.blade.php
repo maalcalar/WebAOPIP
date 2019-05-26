c5d432fb8cdac387a02accacf85e3da696aacc5e
Miguel Angel Alcalá Rios <miguel.alcala.rios@gmail.com>
2019-05-26 18:29:29 -0500

    [INFO]-Layout para toda la web.

---
+++ resources/layouts/app.blade.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>The Spice Lounge</title>
+<meta name="description" content="The Spice Lounge" />
+<meta name="keywords" content="The Spice Lounge" />
+<meta name="author" content="The Spice Lounge" />
+
+<!-- Loading Google Web fonts-->
+<link href='css/fonts.googleapis.CarroisGothicSC.css' rel='stylesheet' type='text/css' />
+<link href='css/font.googleapis.Raleway.css' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="css/bootstrap.css" type="text/css"/>
+<link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css" type="text/css"/>
+<link rel="stylesheet" href="css/font-awesome.css" type="text/css" />
+<link rel="stylesheet" href="css/animate.min.css" type="text/css" />
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+
+<!--Menu-->
+<link rel="stylesheet" href="css/menu.css" type="text/css" />
+<!--SlickNavigation For Mobile Device-->
+<link rel="stylesheet" href="css/slicknav.css">
+<!--SlickNavigation For Mobile Device End-->
+<!--Menu End-->
+
+<!--Optional Css-->
+
+<!--Home Tiled Slider-->
+<link rel="stylesheet" href="css/component.css" type="text/css" />
+<!--Home Tiled Slider End-->
+
+<!--Optional Css-->
+
+<!--Color Change-->
+<link rel="stylesheet" href="css/colors/color1.css" id="color" type="text/css" />
+<!--Color Change End-->
+
+
+
+<link rel="shortcut icon" href="images/favicon.ico.png" /> 
+<link rel="apple-touch-icon" href="images/apple_touch_icon.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="images/apple_touch_icon_72x72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="images/apple_touch_icon_114x114.png" />
+
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/jquery-migrate-1.2.1.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+	
+</head>
+<body>
+
+
+
+<!--java script-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- Form Validation-->
+<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+<!-- Form Validation End-->
+<script type="text/javascript" src="js/bootstrap-datetimepicker.min.js"></script>
+<script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+
+<!-- SlickNavigation For Mobile Device-->
+<script type="text/javascript" src="js/jquery.slicknav.min.js"></script>
+<!-- SlickNavigation For Mobile Device Ends-->
+
+<!-- Content NiceScroll -->
+<script src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/jquery.mousewheel.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<!-- Content NiceScroll End-->
+
+<!-- include retina js -->
+<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
+<!-- include retina js Ends-->
+
+<!-- Optional Scripts Starts -->
+
+<!-- Preloader Starts -->
+<script type="text/javascript" src="js/jpreloader.min.js"></script>
+<!-- Preloader End -->
+
+<!-- Home Tiled Sldier -->
+<script type="text/javascript" src="js/classie.js"></script>
+<script type="text/javascript" src="js/boxesFx.js"></script>
+<script type="text/javascript" src="js/wait.js"></script>
+<!-- Home Tiled Sldier End-->
+
+<!-- Optional Scripts Ends -->
+
+<!-- Custom Scripts -->
+<script type="text/javascript" src="js/custom_general_box.js"></script>
+<script type="text/javascript" src="js/custom_general.js"></script>
+<!-- Custom Scripts End-->
+
+</body>
+
+</html>

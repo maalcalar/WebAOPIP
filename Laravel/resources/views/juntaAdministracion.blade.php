3a269158c0ff05c781f8ae72a41cc6ead3acdbb1
Miguel Angel Alcalá Rios <miguel.alcala.rios@gmail.com>
2019-09-17 17:33:08 -0500

    [INFO]-Se agregó el listado de presidentes antecesores.

--- Laravel/resources/views/juntaAdministracion.blade.php
+++ Laravel/resources/views/juntaAdministracion.blade.php
@@ -154,6 +154,133 @@
                                 </table>
                             </div>
                         </div>
+                        <br/>
+                        <h3>Presidentes AOPIP</h3>
+                        <div class="clearfix pad_top13">
+                            <div class="col-md-12">
+                                <table class='table'>
+                                    <head>
+                                        <tr>
+                                            <th>Grados</th>
+                                            <th>Apellidos y Nombres</th>
+                                            <th>Periodo</th>
+                                        </tr>
+                                    </head>
+                                    <body>
+                                        <tr>
+                                            <td>Coronel PNP</td>
+                                            <td>Barrera Barreno Cesar Hilario</td>
+                                            <td>2017-2018</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Mayor PNP</td>
+                                            <td>Clavo Garcia Eleodoro</td>
+                                            <td>2015-2016</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Velazquez Borja Jose</td>
+                                            <td>2013-2014</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Pozo Cardenas Mauro</td>
+                                            <td>2011-2012</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Coronel PNP</td>
+                                            <td>Alva Sanchez Juan</td>
+                                            <td>2009-2010</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Del Aguila Baluarte Juan</td>
+                                            <td>2007-2008</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Coronel PNP</td>
+                                            <td>Ruiz Camacho Agustin</td>
+                                            <td>2005-2006</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Coronel PNP</td>
+                                            <td>Jimenez Bacca Benedicto</td>
+                                            <td>2003-2004</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Chong Ching Ramon</td>
+                                            <td>2001-2002</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Capitán PNP</td>
+                                            <td>Burgos Torres Wiliam</td>
+                                            <td>1999-2000</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Succlla Flores Edgar</td>
+                                            <td>1997-1998</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Lopez Bolivar Rogelio</td>
+                                            <td>1995-1996</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Coronel PNP</td>
+                                            <td>Santander Bueno Luis Antonio</td>
+                                            <td>1993-1994</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Villanueva Aguirre Andres</td>
+                                            <td>1991-1992</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Peralta Sanchez Gustavo</td>
+                                            <td>1989-1990</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Froilan Palacios Hernandez</td>
+                                            <td>1987-1988</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Teniente General PNP</td>
+                                            <td>Baca Valdivia Berly</td>
+                                            <td>1985-1986</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Saldivar Campos Augurio</td>
+                                            <td>1984</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Coahila De La Peña Hugo</td>
+                                            <td>1982-1983</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Teniente General PNP</td>
+                                            <td>Muñoz Malaver Mario</td>
+                                            <td>1978-1979</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Vega Iriarte Jesus</td>
+                                            <td>1974-1975</td>
+                                        </tr>
+                                        <tr>
+                                            <td>General PNP</td>
+                                            <td>Rosado Oviedo Francisco</td>
+                                            <td>1972-1973</td>
+                                        </tr>
+                                    </body>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>

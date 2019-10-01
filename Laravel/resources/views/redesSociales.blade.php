2fa7168a7a1d4b7be8abf0db0151cb5f33db4a39
Miguel Angel Alcalá Rios <miguel.alcala.rios@gmail.com>
2019-10-01 16:55:20 -0500

    [INFO]-Links de la redes sociales.

---
+++ Laravel/resources/views/redesSociales.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('title', 'Redes Sociales')
+
+@section('contenido')
+<div id="about" class="item fullwidth">
+	<div class="full-width img-overlay"></div>
+    <div class="content">
+        <div class="content_overlay_fullwidth"></div>
+        <div class="content_inner">
+            <div class="row contentscroll">
+                <div class="container">
+                    <div class="col-md-12 content_text">
+                        <h1>Nuestras Redes Sociales</h1>
+                        <div class="clearfix pad_top13">
+                            <div class='col-md-4'>
+                                <div class="clearfix location_content">
+                                    <div class="row col-md-12">
+                                        <div class="location-address-wrap">
+                                            <a href="https://www.facebook.com/ClubAopip" target="_blank"><h3 class="border_bottom"><i class="fa fa-facebook"></i>Facebook</h3></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-md-4'>
+                                <div class="clearfix location_content">
+                                    <div class="row col-md-12">
+                                        <div class="location-address-wrap">
+                                            <a href="https://www.instagram.com/clubaopip" target="_blank"><h3 class="border_bottom"><i class="fa fa-instagram"></i>Instagram</h3></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-md-4'>
+                                <div class="clearfix location_content">
+                                    <div class="row col-md-12">
+                                        <div class="location-address-wrap">
+                                            <a href="https://www.youtube.com/channel/UCNYnXPJmP0RyFeQpAfuXn7A" target="_blank"><h3 class="border_bottom"><i class="fa fa-youtube"></i>Youtube</h3></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class='col-md-4'>
+                                <div class="clearfix location_content">
+                                    <div class="row col-md-12">
+                                        <div class="location-address-wrap">
+                                            <a href="https://twitter.com/CAopip" target="_blank"><h3 class="border_bottom"><i class="fa fa-twitter"></i>Twitter</h3></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
